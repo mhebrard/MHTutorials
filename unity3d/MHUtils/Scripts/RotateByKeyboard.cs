0af7f125c6f5af88508b2c487f9d4a76156037cd
mhebrard <maxime.hebrard@gmail.com>
2016-12-19 14:48:46 +0900

    Add Unity3D VRCamera + MoveCamera

---
+++ unity3d/MHUtils/Scripts/RotateByKeyboard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MHUtils
+{
+    public class RotateByKeyboard : MonoBehaviour
+    {
+        [SerializeField]
+        private float speed = 5;
+
+        // Update is called once per frame
+        void Update()
+        {
+            // arrows + wsad + joystick
+            float h = Input.GetAxis("Horizontal") * speed;
+            float v = Input.GetAxis("Vertical") * speed * -1; // inverte vertical axis
+            transform.Rotate(v, h, 0);
+        }
+    }
+}
